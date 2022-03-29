62e8919316ac9203c4492e8b61af64329620992d
koteswari6170 <102402089+koteswari6170@users.noreply.github.com>
2022-03-29 10:35:08 +0530

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,58 @@
+# a=4
+# print(type(a)) # int
+# print(float(a)) #4.0
+# print(str(a)) # 4
+# b=7.5
+# print(type(b)) #float
+# print(int(b)) # 7
+# print(str(b)) #7.5
+# a=3
+# print("the value of a is",a) # the value of a is 3
+# print("the value of a is ",a) # the value of a is  3
+# print("the value of a is"+a) # can only concatenate str (not int ) to str
+# print("the value of a is"+'a') # the value of a isa
+# print("the value of a is"+' '+str(a)) # the value of a is 3
+# b=""
+# print(b,type(b)) #str
+# c=""
+# print(c.isspace()) #False
+# d="  "
+# print(d.isspace()) #True
+# d=5
+# print(type(d)) #int
+# print(int(d)) #5
+# print(float(d)) #5.0
+# print(str(d)) #5
+# d='DELL'
+# print("one of the laptop brand is",d) # one of the laptop brand is DELL
+# print(''+d) # DELL
+# print(' '+d) #  DELL
+# print("one of the laptop brand is"+d) #one of the laptop brand isDELL
+# print("one of the laptop brand is"+' '+d) #one of the laptop brand is DELL
+# d="12"
+# print(d,type(d)) #str
+# print(int(d)) #12
+# print(float(d))#12.0
+# print(int(5.7)) #5
+# print(float(4)) #4.0
+# print(int("python")) #valueerror
+# print(float("python")) #valueerror
+c='56.75'
+# print(c,type(c)) #str
+d=float(c)
+print(d) #56.75
+# print(int(c)) #not possible
+print(int(d)) #56
+
+
+
+
+
+
+
+
+
+
+
+
+
